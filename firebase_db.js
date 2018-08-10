bc125fe72b069cba78345422fc37863bd9a42c96
sanket0896 <sanket14mishra@gmail.com>
2018-08-10 18:01:02 +0530

    database initialise

---
+++ firebase_db.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB7jadZ3NSCuGG47IFMFFzpQqMZH45obc0",
+    authDomain: "social-profile-mindfire.firebaseapp.com",
+    databaseURL: "https://social-profile-mindfire.firebaseio.com",
+    projectId: "social-profile-mindfire",
+    storageBucket: "social-profile-mindfire.appspot.com",
+    messagingSenderId: "845264960637"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
