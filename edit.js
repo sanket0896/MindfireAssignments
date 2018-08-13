1f5bf4a92c21563afcd73d95dd21ec17b300b281
sanket0896 <sanket14mishra@gmail.com>
2018-08-13 15:52:22 +0530

    Basic Functionalities complete

--- edit.js
+++ edit.js
@@ -1,16 +1,130 @@
 // initialise session storage or local storage
 
-let wStorage = window.sessionStorage;
+let wStorage = window.localStorage;
+
+let personDetails = {
+    "personalDetails" : {
+        "dpURLSelf" : "#",
+        "name": "",
+        "dob": "",
+        "city": "",
+        "company": "",
+        "facebookURL": "",
+        "twitterURL": "",
+        "githubURL": "",
+        "linkedinURL": ""
+    },
+    "bioDetails" : "",
+    "friendDetails" : [],
+    "likeDetails" : [],
+    "interestDetails" : []
+};
+
+function getDataFromStorage(key) {
+    if (wStorage.getItem(key)) {
+        value = JSON.parse(wStorage.getItem(key));
+        return value;
+    }
+    else{
+        return personDetails;
+    }
+}
 
 //on form submit, create JSON object and write JSON to session
 
-document.getElementById("modal-form-dp").addEventListener("submit", (e)=>{
+$("#modal-form-dp").on("submit", function (e) {
     e.preventDefault();
-    
-    let inp = e.target.elements;
-    for (let i = 0; i < inp.length; i++) {
-        console.log(inp[i]);
-                
-    }
+    let currentPerson = getDataFromStorage("personDetails");
+    currentPerson.personalDetails.dpURLSelf = e.target.elements[0].value;
+    // e.target.elements[0].value = "";
+    $("#dp-upload-modal").modal('hide');
+    wStorage.setItem("personDetails",JSON.stringify(currentPerson));
     return false;
 });
+
+$("#page-form-personal-details").on("submit", function (e) {
+    e.preventDefault();
+    let currentPerson = getDataFromStorage("personDetails");
+    currentPerson.personalDetails.name = e.target.elements[0].value;
+    // e.target.elements[0].value = "";
+    currentPerson.personalDetails.dob = e.target.elements[1].value;
+    // e.target.elements[1].value = "";
+    currentPerson.personalDetails.city = e.target.elements[2].value;
+    // e.target.elements[2].value = "";
+    currentPerson.personalDetails.company = e.target.elements[3].value;
+    // e.target.elements[3].value = "";
+    currentPerson.personalDetails.facebookURL = e.target.elements[4].value;
+    // e.target.elements[4].value = "";
+    currentPerson.personalDetails.twitterURL = e.target.elements[5].value;
+    // e.target.elements[5].value = "";
+    currentPerson.personalDetails.githubURL = e.target.elements[6].value;
+    // e.target.elements[6].value = "";
+    currentPerson.personalDetails.linkedinURL = e.target.elements[7].value;
+    // e.target.elements[7].value = "";
+    console.log(currentPerson);
+    wStorage.setItem("personDetails",JSON.stringify(currentPerson));
+    return false;
+});
+
+$("#page-form-bio").on("submit", function (e) {
+    e.preventDefault();
+    let currentPerson = getDataFromStorage("personDetails");
+    currentPerson.bioDetails = e.target.elements[0].value;
+    console.log(currentPerson);
+    wStorage.setItem("personDetails",JSON.stringify(currentPerson));
+    return false;
+})
+
+$("#modal-form-friend").on("submit", function (e) {
+    e.preventDefault();
+    let currentPerson = getDataFromStorage("personDetails");
+    let currentFriend = {
+        "imageURL": e.target.elements[0].value,
+        "name": e.target.elements[1].value
+    }
+    currentPerson.friendDetails.push(currentFriend);
+    e.target.elements[0].value = "";
+    e.target.elements[1].value = "";
+    $("#friend-upload-modal").modal('hide');
+    console.log(currentPerson);    
+    wStorage.setItem("personDetails",JSON.stringify(currentPerson));
+    return false;
+});
+
+$("#modal-form-like").on("submit", function (e) {
+    e.preventDefault();
+    let currentPerson = getDataFromStorage("personDetails");
+    let currentLike = {
+        "imageURL": e.target.elements[0].value,
+        "name": e.target.elements[1].value
+    }
+    currentPerson.likeDetails.push(currentLike);
+    e.target.elements[0].value = "";
+    e.target.elements[1].value = "";
+    $("#like-upload-modal").modal('hide');
+    console.log(currentPerson);    
+    wStorage.setItem("personDetails",JSON.stringify(currentPerson));
+    return false;
+});
+
+$("#modal-form-interest").on("submit", function (e) {
+    e.preventDefault();
+    let currentPerson = getDataFromStorage("personDetails");
+    let currentInterest = {
+        "imageURL": e.target.elements[0].value,
+        "name": e.target.elements[1].value
+    }
+    currentPerson.interestDetails.push(currentInterest);
+    e.target.elements[0].value = "";
+    e.target.elements[1].value = "";
+    $("#interest-upload-modal").modal('hide');
+    console.log(currentPerson);    
+    wStorage.setItem("personDetails",JSON.stringify(currentPerson));    
+    return false;
+});
+
+document.getElementById("delete-user").addEventListener("click",function () {
+    wStorage.removeItem("personDetails");
+    alert("User deleted. Please fill the details again.");
+    window.open("/edit.html","_self");
+})
--- main.js
+++ main.js
@@ -1,30 +1,98 @@
-function generateElementNode(elementId,domElementStr,data) {
-  domElement = createHTML(domElementStr);
+let wStorage = window.localStorage;
+let personDetails = wStorage.getItem("personDetails");
+
+function createHTML(htmlStr) {
+  var tempContainer = document.createElement('div');
+  htmlStr = htmlStr.trim();
+  tempContainer.innerHTML = htmlStr;
+  var htmlNode = tempContainer.firstChild;
+  console.log(htmlNode);
+  
+  return htmlNode;
+}
+
+function generateElementNode(elementId,data) {
+  let domElementStr,domElement;
   switch (elementId) {
-    case "friends-list":
-    case "likes-list":
-    case "interests-list":
-      domElement.querySelectorAll("[data-desc = image]")[0].src = "https://picsum.photos/200?random";
-      domElement.querySelectorAll("[data-desc = text]")[0].innerHTML = "Friend "+data;
-      break;
-    case "bio-text":
-      domElement.querySelectorAll("[data-desc = text]")[0].innerHTML = "kajsndkasndjk ajdsnkandsa ksdjas dkasjd kdansd cjhsa j acj."+data;
-      break;
-    default:
-      break;
+    case "friends-list": domElementStr = `<li class="col-6 col-sm-3 col-lg-2 mr-0 list-inline-item">
+                                            <img src="" alt="" class="img-75" data-desc="image">
+                                            <p id="text class="text-justify" data-desc="text"></p>
+                                          </li>`;
+                        domElement = createHTML(domElementStr);
+                        domElement.querySelectorAll("[data-desc = image]")[0].src = data.imageURL;
+                        // "https://picsum.photos/200?random";
+                        domElement.querySelectorAll("[data-desc = text]")[0].innerHTML = data.name;
+                        break;
+    case "likes-list": domElementStr = `<li class="col-6 col-sm-3 col-lg-2 mr-0 list-inline-item">
+                                          <img src="" alt="" class="img-75" data-desc="image">
+                                          <p id="text class="text-justify" data-desc="text"></p>
+                                        </li>`;
+                      domElement = createHTML(domElementStr);
+                      domElement.querySelectorAll("[data-desc = image]")[0].src = data.imageURL;
+                      // "https://picsum.photos/200?random";
+                      domElement.querySelectorAll("[data-desc = text]")[0].innerHTML = data.name;
+                      break;
+    case "interests-list": domElementStr = `<li class="col-6 col-sm-3 col-lg-2 mr-0 list-inline-item">
+                                              <img src="" alt="" class="img-75" data-desc="image">
+                                              <p id="text class="text-justify" data-desc="text"></p>
+                                            </li>`;
+                          domElement = createHTML(domElementStr);
+                          domElement.querySelectorAll("[data-desc = image]")[0].src = data.imageURL;
+                          // "https://picsum.photos/200?random";
+                          domElement.querySelectorAll("[data-desc = text]")[0].innerHTML = data.name;
+                          break;
+default:
+    break;
   }
   return domElement;
 }
 
-function populateDataInElement(elementId,domElementStr,data) {
-  dataLength = data.length;
-  // dataLength = 6;//used for testing
-  for (let index = 0; index < dataLength; index++) {
-    domElement = generateElementNode(elementId,domElementStr,index)
-    document.getElementById(elementId).appendChild(domElement);  
+function populateDataInElement(elementId,data,isLink=false) {
+  console.log(isLink);
+  
+  if (Array.isArray(data)) {
+    data.forEach(function (dataInstance) {
+      let domElement = generateElementNode(elementId,dataInstance)
+      document.getElementById(elementId).appendChild(domElement);  
+    });
+  }
+  else if(isLink === true) {
+      if (elementId === "dp-self") {
+        document.getElementById(elementId).src = data;        
+      }
+      else{
+        document.getElementById(elementId).href = data;
+      }
+      return;
+    }
+  else{
+    document.getElementById(elementId).innerHTML=data;
   }
 }
-populateDataInElement("friends-list",friendElementStr,[{},{},{},{},{},{}]);
-populateDataInElement("likes-list",friendElementStr,[{},{},{},{},{},{}]);
-populateDataInElement("interests-list",friendElementStr,[{},{},{},{},{},{}]);
-populateDataInElement("bio-text",bioElementStr,[{}]);
+
+if (personDetails) {
+  personDetails = JSON.parse(personDetails);
+  populateDataInElement("dp-self", "https://picsum.photos/100?random", true);
+  populateDataInElement("name-self", personDetails.personalDetails.name);
+  populateDataInElement("dob-self", personDetails.personalDetails.dob);
+  populateDataInElement("city-self", personDetails.personalDetails.city);
+  populateDataInElement("company-self", personDetails.personalDetails.company);
+  populateDataInElement("facebook-self", personDetails.personalDetails.facebookURL,true);
+  populateDataInElement("twitter-self", personDetails.personalDetails.twitterURL,true);
+  populateDataInElement("github-self", personDetails.personalDetails.githubURL,true);
+  populateDataInElement("linkedin-self", personDetails.personalDetails.linkedinURL,true);
+  populateDataInElement("bio-text", personDetails.bioDetails);
+  populateDataInElement("friends-list", personDetails.friendDetails);
+  populateDataInElement("likes-list", personDetails.likeDetails);
+  populateDataInElement("interests-list", personDetails.interestDetails);
+}
+else{
+  alert("User details doesn't exist in records. Please fill the details first.");
+  window.open("/edit.html","_self");
+}
+
+document.getElementById("delete-user").addEventListener("click",function () {
+  wStorage.removeItem("personDetails");
+  alert("User deleted. Please fill the details again.");
+  window.open("/edit.html","_self");
+})
