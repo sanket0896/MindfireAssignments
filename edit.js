ae4b0d13b98c68a6a02bd26d9fdd49d59ad93f53
Sanket Mishra <sanket14mishra@gmail.com>
2018-08-13 11:25:55 +0530

    Changed saving to session storage

--- edit.js
+++ edit.js
@@ -1,15 +1,16 @@
-var dpSelf = document.getElementById("dp-self");
-function uploadFile(file,path){
-    var filename = file.name;
-    // var uploadTask = storageRef.child(path+'/'+filename).put(file);
-    console.log(filename+"  "+path);
-    
-}
+// initialise session storage or local storage
 
-dpSelf.addEventListener("change",function (e) {
-    var selectedFile = e.target.files[0];
-    // get path from the type of html modal
-    // path = displayPic,friendPic,likePic,interestPic
-    var path = "displayPic";
-    uploadFile(selectedFile,path);
-})
+let wStorage = window.sessionStorage;
+
+//on form submit, create JSON object and write JSON to session
+
+document.getElementById("modal-form-dp").addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+    let inp = e.target.elements;
+    for (let i = 0; i < inp.length; i++) {
+        console.log(inp[i]);
+                
+    }
+    return false;
+});
--- firebase_db_edit.js
+++ firebase_db_edit.js
@@ -1,9 +1,21 @@
 var database = firebase.database();
 var storage = firebase.storage();
 var storageRef = storage.ref();
-function uploadFile(file,path){
-    var filename = file.name;
-    var uploadTask = storageRef.child(path+'/'+filename).put(file);
-    console.log(filename+"  "+path);
+
+function saveURLToDB(url) {
+    //save URL to db
+    console.log(url);
     
+}
+
+function uploadFile(file,path){
+    // give a better name or folder structure to db
+    var filename = new Date() + file.name;
+    var uploadTask = storageRef.child(path+'/'+filename).put(file);
+
+    uploadTask.then(function (snapshot) {
+        snapshot.ref.getDownloadURL().then(function (url) {
+            saveURLToDB(url);
+        })
+    });
 }
